a0a2534f6814dac2dfc05d7361c5a29df16b2537
Ilshat Osmanov <190103274@stu.sdu.edu.kz>
2023-11-18 02:22:18 +0600

    add nav controller

--- app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
+++ app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -15,13 +15,24 @@
  */
 package com.example.lunchtray
 
+import androidx.annotation.StringRes
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import com.example.lunchtray.ui.OrderViewModel
+
+enum class LunchTrayScreen(@StringRes val title: Int) {
+    Start(title = R.string.app_name),
+    Entree(title = R.string.choose_entree),
+    SideDish(title = R.string.choose_side_dish),
+    Accompaniment(title = R.string.choose_accompaniment),
+    Checkout(title = R.string.order_checkout)
+}
 
 // TODO: Screen enum
 
@@ -31,6 +42,7 @@ import com.example.lunchtray.ui.OrderViewModel
 @Composable
 fun LunchTrayApp() {
     // TODO: Create Controller and initialization
+    val navController = rememberNavController()
 
     // Create ViewModel
     val viewModel: OrderViewModel = viewModel()
@@ -43,5 +55,11 @@ fun LunchTrayApp() {
         val uiState by viewModel.uiState.collectAsState()
 
         // TODO: Navigation host
+        NavHost(
+            navController = navController,
+            startDestination = LunchTrayScreen.Start.name,
+        ) {
+
+        }
     }
 }
